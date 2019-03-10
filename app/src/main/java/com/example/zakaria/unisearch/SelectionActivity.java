010c57914115938813b6abfb27db0d805392275e
kashifqamarmalik <kashifqamar111@gmail.com>
2019-03-10 22:43:38 +0200

    Adding the functionality via Java

--- app/src/main/java/com/example/zakaria/unisearch/SelectionActivity.java
+++ app/src/main/java/com/example/zakaria/unisearch/SelectionActivity.java
@@ -2,12 +2,5 @@ package com.example.zakaria.unisearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-public class SelectionActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selection);
-    }
-}
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
