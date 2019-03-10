3671d44ab3a735843dd1c3f0bfb5e606d2d7d226
kashifqamarmalik <kashifqamar111@gmail.com>
2019-03-10 22:44:19 +0200

    List of countries added

--- app/src/main/java/com/example/zakaria/unisearch/SelectionActivity.java
+++ app/src/main/java/com/example/zakaria/unisearch/SelectionActivity.java
@@ -4,3 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+public class SelectionActivity extends AppCompatActivity {
+
+    String [] COUNTRYLIST = {"Select Country","Finland", "Norway", "Sweden", "Denmark", "Pakistan", "Bangladesh"};
