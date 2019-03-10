f2d3f6c87ca32551f7e6d621b7d57e5993ad1013
kashifqamarmalik <kashifqamar111@gmail.com>
2019-03-10 22:45:00 +0200

    List of cities added

--- app/src/main/java/com/example/zakaria/unisearch/SelectionActivity.java
+++ app/src/main/java/com/example/zakaria/unisearch/SelectionActivity.java
@@ -7,3 +7,8 @@ import android.widget.Spinner;
 public class SelectionActivity extends AppCompatActivity {
 
     String [] COUNTRYLIST = {"Select Country","Finland", "Norway", "Sweden", "Denmark", "Pakistan", "Bangladesh"};
+    String [] CITYLIST = {"Select City","Helsinki", "Turku", "Tampere", "Oulu"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection);
