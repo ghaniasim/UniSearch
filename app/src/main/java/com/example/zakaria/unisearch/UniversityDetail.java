034d9f4f9375b4cee25f291e9a665d19bb4086a3
ghaniasim <ghaniasimm@gmail.com>
2019-03-12 00:09:02 +0200

    image and toast message

--- app/src/main/java/com/example/zakaria/unisearch/UniversityDetail.java
+++ app/src/main/java/com/example/zakaria/unisearch/UniversityDetail.java
@@ -3,10 +3,14 @@ package com.example.zakaria.unisearch;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class UniversityDetail extends AppCompatActivity {
+
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,16 @@ public class UniversityDetail extends AppCompatActivity {
         ((TextView)findViewById(R.id.textViewTitle))
                 .setText(UniversityList.getInstance().getUniversity(i).getName());
         ((ImageView)findViewById(R.id.imageView)).setImageResource(UniversityList.getInstance().getUniversity(i).getImage());
+
+        image = (ImageView) findViewById(R.id.favouriteImageView);
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image.setImageResource(R.drawable.ic_favorite_black_24dp);
+                Toast.makeText(getApplicationContext(), "Added to favourites!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
