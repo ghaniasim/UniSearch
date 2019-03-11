11fcd7feba50394d985ddef996d70b8976d38e84
ghaniasim <ghaniasimm@gmail.com>
2019-03-11 16:42:14 +0200

    ready to jump between fragments

--- app/src/main/java/com/example/zakaria/unisearch/ResultActivity.java
+++ app/src/main/java/com/example/zakaria/unisearch/ResultActivity.java
@@ -1,13 +1,57 @@
 package com.example.zakaria.unisearch;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private ListFragment listFragment;
+    private FavouritesFragment favouritesFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        listFragment = new ListFragment();
+        favouritesFragment = new FavouritesFragment();
+
+        setFragment(listFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.nav_list:
+                        setFragment(listFragment);
+                        return true;
+                    case R.id.nav_favourites:
+                        setFragment(favouritesFragment);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
