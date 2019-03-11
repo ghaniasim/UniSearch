1ddc3a9f750f1fd3861ff7e342b245e81a0a0d80
ghaniasim <ghaniasimm@gmail.com>
2019-03-11 23:39:31 +0200

    detail activity ready

--- app/src/main/java/com/example/zakaria/unisearch/UniversityList.java
+++ app/src/main/java/com/example/zakaria/unisearch/UniversityList.java
@@ -14,16 +14,16 @@ public class UniversityList {
 
     public void add() {
         universities = new ArrayList<University>();
-        universities.add(new University("University of Helsinki", R.drawable.metropolia));
+        universities.add(new University("University of Helsinki", R.drawable.helsinki));
         universities.add(new University("Metropolia UAS", R.drawable.metropolia));
-//        universities.add(new University("Haaga-Helia UAS"));
+        universities.add(new University("Haaga-Helia UAS", R.drawable.haagahelia));
         universities.add(new University("Aalto University", R.drawable.aalto));
-//        universities.add(new University("Laurea UAS"));
-//        universities.add(new University("Diaconia UAS"));
-//        universities.add(new University("Humak UAS"));
-//        universities.add(new University("Helsinki School of Business"));
-//        universities.add(new University("National Defense University"));
-//        universities.add(new University("Helsinki Design School"));
+        universities.add(new University("Laurea UAS", R.drawable.laurea));
+        universities.add(new University("Diaconia UAS", R.drawable.diconia));
+        universities.add(new University("Humak UAS", R.drawable.humak));
+        universities.add(new University("Helsinki School of Business", R.drawable.helsinkibusiness));
+        universities.add(new University("National Defense University", R.drawable.defense));
+        universities.add(new University("Helsinki Design School", R.drawable.desing));
     }
 
     public List<University> getUniversityList() {
